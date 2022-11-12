1d5bb85d0a60a247051b0bd5c26d3efb475327fa
Patrick Li <1543162420@qq.com>
2022-11-11 19:03:16 -0500

    feat: filter the links

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, Response, redirect
 import requests
+import os
 
 app = Flask(__name__)
+serverPath = os.environ.get('SERVER_PATH', 'http://localhost:5000')
 
 def filt(data: str):
     data = data.replace("Salty", "Fake")
     data = data.replace("salty ", "fake ")
+    data = data.replace("https://im.salty.fish", serverPath)
+    data = data.replace("im.salty.fish", "localhost:5000")
     return data
 @app.route("/<path:path>")
 def hello_world(path: str):
-    data = requests.get(f"https://im.salty.fish/{path}").text
-    return filt(data)
+    if path.endswith(".png") or path.endswith(".jpg"):
+        return redirect("https://im.salty.fish/" + path)
+    data = requests.get(f"https://im.salty.fish/{path}")
+    content_type = data.headers["Content-Type"]
+    return Response(filt(data.text), content_type=content_type)
 
 @app.route("/")
 def empty():
